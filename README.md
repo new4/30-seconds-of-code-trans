9ab25ef0498a4933057de971b0a057017989f628
new4 <new4wang@163.com>
2019-01-02 12:57:23 +0800

    准备工作

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 30-seconds-of-code-trans
-reading 30-seconds-of-code
+
+闲暇阅读 [30-seconds-of-code](https://github.com/30-seconds/30-seconds-of-code) 里的代码。
---
+++ build/extract.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+const {
+  flatten,
+  toLower,
+} = require('lodash');
+
+const objectFromPairs = arr => arr.reduce((acc, curValue) => ((acc[curValue[0]] = curValue[1]), acc), {}); // eslint-disable-line
+
+const strSortFn = (a, b) => {
+  a = toLower(a);
+  b = toLower(b);
+  if (a < b) { return -1; }
+  if (a > b) { return 1; }
+  return 0;
+};
+
+const TAG_DB_PATH = path.resolve(__dirname, '../tag_database');
+const OUTPUT = path.resolve(__dirname, '../db.yml');
+
+const readTags = () => {
+  let tagDbData = {};
+  try {
+    tagDbData = objectFromPairs(
+      fs
+        .readFileSync(TAG_DB_PATH, 'utf8')
+        .split('\n')
+        .filter(v => v.trim() !== '')
+        .map((v) => {
+          const data = v.split(':').slice(0, 2);
+          data[1] = data[1].split(',').map(t => t.trim());
+          return data;
+        }),
+    );
+  } catch (err) {
+    console.error(`ERROR! During tag database loading: ${err}`);
+    process.exit(1);
+  }
+  return tagDbData;
+};
+
+const tagsInfo = readTags();
+
+const obj = {};
+Object
+  .entries(tagsInfo)
+  .forEach(([key, [tag]]) => {
+    if (!obj[tag]) {
+      obj[tag] = [];
+    }
+    obj[tag].push(key);
+  });
+
+const strArr = Object.keys(obj).sort(strSortFn)
+  .map((tag) => {
+    const temp = obj[tag].map(name => `  - ${name}`);
+    temp.sort(strSortFn).unshift(`${tag}:`);
+    return temp;
+  });
+
+fs.writeFileSync(OUTPUT, flatten(strArr).join('\n'));
+
